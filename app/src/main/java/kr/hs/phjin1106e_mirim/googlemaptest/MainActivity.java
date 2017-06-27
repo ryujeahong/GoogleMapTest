c39fa65257c45904fbf6641f5b9f55d20ab95093
ryujeahong <phnin1106@naver.com>
2017-06-27 20:36:23 +0900

    -last

--- app/src/main/java/kr/hs/phjin1106e_mirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kr/hs/phjin1106e_mirim/googlemaptest/MainActivity.java
@@ -2,12 +2,74 @@ package kr.hs.phjin1106e_mirim.googlemaptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.839269, 128.631892), 17));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                loc_mark=new GroundOverlayOptions();
+                loc_mark.image(BitmapDescriptorFactory.fromResource(R.drawable.icon)).position(latLng, 100f, 100f);
+                googleMap.addGroundOverlay(loc_mark);
+            }
+        });
+    }
+
+    public static final int ITEM_SATELLITE=1;
+    public static final int ITEM_NORMAL=2;
+    public static final int ITEM_COEXMALL=3;
+    public static final int ITEM_IPARKMALL=4;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, ITEM_SATELLITE, 0, "위성 지도");
+        menu.add(0, ITEM_NORMAL, 0, "일반 지도");
+        SubMenu hotMenu=menu.addSubMenu("핫 플레이스");
+        hotMenu.add(0, ITEM_COEXMALL, 0, "코엑스몰");
+        hotMenu.add(0, ITEM_IPARKMALL,0,"아이파크몰");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case ITEM_SATELLITE: googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case ITEM_NORMAL: googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case ITEM_COEXMALL: googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.513287, 127.057760), 17));
+                return true;
+            case ITEM_IPARKMALL : googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.529084, 126.963991),17));
+        }
+        return false;
     }
 }
